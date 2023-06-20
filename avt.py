f5a3500f9d10f580cd63006c3116a5d1550c3cf0
Kirukatoya <126419188+Kirukatoya@users.noreply.github.com>
2023-06-20 06:40:31 +0300

    add dipl1 add d

--- avt.py
+++ avt.py
@@ -1,21 +1,25 @@
 import math
-
+from PyQt5.QtGui import QDoubleValidator
 from PySide2 import QtCore
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
 
-#import pro
-#from ui_pro import *
+# import pro
+# from ui_pro import *
 
 qt_creator = "pro.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qt_creator)
+
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
-
-        #self.ui = Ui_MainWindow()
         self.setupUi(self)
+        validator = QDoubleValidator()
+        self.lineEdit_7.setValidator(validator)
+        self.lineEdit_6.setValidator(validator)
+        self.lineEdit_8.setValidator(validator)
 
         # установить event filter для кнопок
         self.HelpBtn.installEventFilter(self)
@@ -25,7 +29,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.OkBtn.installEventFilter(self)
         self.clearBtn.installEventFilter(self)
         self.backBtn.installEventFilter(self)
-
 
     def eventFilter(self, source, event):
         if source == self.HelpBtn and event.type() == QtCore.QEvent.MouseButtonPress:
@@ -53,8 +56,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             else:
                 print("Пожалуйста заполните поля ввода")
 
-                self.textEdit_9.setText("Пожалуйста заполните поля ввода")
-                #self.textEdit_9.setVisible(not self.textEdit_9.isVisible())
+                self.textEdit_9.setText("\n\n\n\n\n\n  Пожалуйста заполните поля ввода")
+                # self.textEdit_9.setVisible(not self.textEdit_9.isVisible())
 
             print("OkBtn event filter")
         if source == self.clearBtn and event.type() == QtCore.QEvent.MouseButtonPress:
@@ -67,22 +70,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         return super().eventFilter(source, event)
 
-
     def ras(self):
         # Получение введенных данных
         per_hour = float(self.lineEdit_7.text())
         service_channels = int(self.lineEdit_6.text())
         service_time = float(self.lineEdit_8.text())
-        per_hour = per_hour/60
+        per_hour = per_hour / 60
         print(per_hour)
         # Расчет показателей
-        service_intensity = (1/service_time)
+        service_intensity = (1 / service_time)
         print(service_intensity)
 
         # Нагрузка системы
-        system_load = per_hour * service_time
+        system_load = per_hour / service_intensity
         if system_load / service_channels < 1:
-            print("нагрузка системы",system_load)
+            print("нагрузка системы", system_load)
+
             # Вероятность простоя системы
 
             def calculate_idle_probability(system_load, service_channels):
@@ -92,46 +95,93 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                     idle_probability += (system_load ** i) / math.factorial(i)
 
                 idle_probability += (system_load ** (service_channels + 1)) / (
-                            math.factorial(service_channels) * (service_channels - system_load))
+                        math.factorial(service_channels) * (service_channels - system_load))
 
                 return (1 / idle_probability)
 
-
-            idle_probability = round((calculate_idle_probability(system_load, service_channels)),3)
-            print("Веростность простоя системы",idle_probability, "%")
+            idle_probability = round((calculate_idle_probability(system_load, service_channels)), 3)
+            print("Веростность простоя системы", idle_probability, "%")
 
             # Вероятность образования очереди
-            poch = round((((system_load**(service_channels+1)) / (math.factorial(service_channels) * (service_channels - system_load)))* idle_probability),2)
-            print("Вероятность образования очереди",poch)
+            poch = round((((system_load ** (service_channels + 1)) / (
+                    math.factorial(service_channels) * (service_channels - system_load))) * idle_probability), 2)
+            print("Вероятность образования очереди", poch)
 
             # Средняя число заявок в очереди
-            avg_queue_length = round((((system_load**(service_channels+1))/(service_channels * math.factorial(service_channels) * (1-(system_load/service_channels))**2))*idle_probability),2)
-            print("Средняя число заявок в очереди",avg_queue_length)
+            avg_queue_length = round((((system_load ** (service_channels + 1)) / (
+                    service_channels * math.factorial(service_channels) * (
+                    1 - (system_load / service_channels)) ** 2)) * idle_probability), 2)
+            print("Средняя число заявок в очереди", avg_queue_length)
 
             # Среднее время ожидания в очереди
-            avg_waiting_time = round((avg_queue_length / per_hour),2)
-            print("Среднее время ожидания в очереди",avg_waiting_time)
+            avg_waiting_time = round((avg_queue_length / per_hour), 2)
+            print("Среднее время ожидания в очереди", avg_waiting_time)
 
             # Среднее число занятых каналолов
             avg_number_channels = system_load
-            print("Число занятых каналов",avg_number_channels)
+            print("Число занятых каналов", avg_number_channels)
 
             # Среднее время прибывания в системе
             sistem_time = avg_waiting_time + service_time
-            print("Среднее время прибывания в системе",sistem_time)
+            print("Среднее время прибывания в системе", sistem_time)
 
             # Коэфициэнт занятых обслуживанием каналов
-            kof = system_load / service_channels
-            print("Коэфициэнт занятых обслуживанием каналов", kof)
+            kof = round((system_load / service_channels), 2)
+            print("Коэффициэнт занятых обслуживанием каналов", kof)
+
+            idle_probability = idle_probability * 100
+            poch = poch * 100
 
             # Вставка рассчитанных показателей
-            self.lineEdit.setText(str(idle_probability))
+            self.lineEdit.setText(str(idle_probability) + "%")
             self.lineEdit_2.setText(str(avg_queue_length))
             self.lineEdit_3.setText(str(avg_waiting_time) + " минут")
             self.lineEdit_4.setText(str(system_load))
-            self.lineEdit_10.setText(str(poch))
+            self.lineEdit_10.setText(str(poch) + "%")
             self.lineEdit_11.setText(str(avg_number_channels))
             self.lineEdit_12.setText(str(sistem_time) + " минут")
+            self.lineEdit_5.setText(str(kof))
+            if poch > 70:
+                self.textEdit.setText("Вам необходимо увеличить кол-во каналов обслуживания, так как показатель среднего числа занятых каналов "
+                                      "указывает на то, что система справляется, но слишком загружена."
+                                      "\nТак же вероятность образования очереди слишком большая, что негативно скажется на посетителях."
+                                      "\nКоэффициент занятых обслуживанием каналов также указывает на большую загрузку системы."
+                                      "\n\nНагрузка системы: Показывает среднее количество запросов, поступающих в систему за единицу времени."
+                                      "\n\nВероятность простоя системы: Вероятность того, что все каналы обслуживания будут свободны в определенный момент времени."
+                                      "\n\nСреднее время ожидания в очереди: Показывает ожидаемое время, которое запросы проводят в очереди перед обслуживанием. "
+                                      "Если среднее время ожидания слишком высоко, это может указывать на необходимость оптимизации системы или "
+                                      "добавления ресурсов для ускорения обработки запросов.")
+
+            elif poch > 50:
+                self.textEdit.setText("Показатель среднего числа занятых каналов "
+                                      "указывает на то, что система справляется с потоком посетителей."
+                                      "\nВероятность образования очереди указывает на отличное функционирование системы."
+                                      "\nКоэффициент занятых обслуживанием каналов также указывает на хорошую работу."
+                                      "\n\nНагрузка системы: Показывает среднее количество запросов, поступающих в систему за единицу времени."
+                                      "\n\nВероятность простоя системы: Вероятность того, что все каналы обслуживания будут свободны в определенный момент времени."
+                                      "\n\nСреднее время ожидания в очереди: Показывает ожидаемое время, которое запросы проводят в очереди перед обслуживанием. "
+                                      "Если среднее время ожидания слишком высоко, это может указывать на необходимость оптимизации системы или "
+                                      "добавления ресурсов для ускорения обработки запросов.")
+            elif poch < 50:
+                self.textEdit.setText("Вам необходимо уменьшить кол-во каналов обслуживания, так как показатель среднего числа занятых каналов"
+                                      "указывает на то, что используются не все каналы обслуживания."
+                                      "\nТак же вероятность образования очереди слишком мала, что также говорит о том, что в системе слишком много "
+                                      "каналов обслуживания."
+                                      "\nКоэффициент занятых обслуживанием каналов также указывает на недостаточную нагрузку."
+                                      "\n\nНагрузка системы: Показывает среднее количество запросов, поступающих в систему за единицу времени."
+                                      "\n\nВероятность простоя системы: Вероятность того, что все каналы обслуживания будут свободны в определенный момент времени."
+                                      "\n\nСреднее время ожидания в очереди: Показывает ожидаемое время, которое запросы проводят в очереди перед обслуживанием. "
+                                      "Если среднее время ожидания слишком высоко, это может указывать на необходимость оптимизации системы или "
+                                      "добавления ресурсов для ускорения обработки запросов.")
+
+
+
+
+
+
+
+
+
 
 
     def clearr(self):
@@ -141,11 +191,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.textEdit_9.clear()
 
 
-
 if __name__ == "__main__":
     import sys
+
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    #window = MainWindow()
+    # window = MainWindow()
     sys.exit(app.exec_())
